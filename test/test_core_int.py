5ef1dc4f5a985cd6f1a1801de3609b731c36bb2d
Qi Hangkong <qihangkong@outlook.com>
2024-06-13 11:15:59 +0800

    add  ut_rcore_iu_alu.sv to pytest

--- test/test_core_int.py
+++ test/test_core_int.py
@@ -9,3 +9,11 @@ def test_core_iu(datafiles, simulator):
     with datafiles.as_cwd():
         subprocess.check_call(['runSVUnit', '-s', simulator, '-o', 'out', '-c', '-debug_access+all', '-t', 'ut_alu_int_unit.sv'])
         expect_testrunner_pass('out/run.log')
+
+@all_files_in_dir('ut/core')
+@all_available_simulators()
+def test_rcore_iu_alu(datafiles, simulator):
+    with datafiles.as_cwd():
+        subprocess.check_call(['runSVUnit', '-s', simulator, '-o', 'out', '-c', '-debug_access+all', '-t', 'ut_rcore_iu_alu.sv'])
+        expect_testrunner_pass('out/run.log')
+

c37ef8c96f5e8169f3ce9d7dc89bb6e91e1bb24b
Qi Hangkong <qihangkong@outlook.com>
2024-06-20 16:18:27 +0800

    fixed: use environment RVGPU_HARDWARE

--- test/soc/run.sh
+++ test/soc/run.sh
@@ -1,7 +1,6 @@
 # echo "\`define RVGPU_HARDWARE_TOPPATH_STR \"${RVGPU_HARDWARE}\"" > project.v
 
 export RVGPU_HARDWARE=/root/hardware
-export RVGPU_HW_TEST_SOC=${RVGPU_HARDWARE}/test/soc
 
 pushd /root/svunit
 source Setup.bsh
--- test/soc/svunit.f
+++ test/soc/svunit.f
@@ -1,4 +1,4 @@
 +incdir+$SVUNIT_INSTALL/svunit_base/util
 
--f ${RVGPU_HW_TEST_SOC}/logical/filelists/sim.fl
+-f ${RVGPU_HARDWARE}/test/soc/logical/filelists/sim.fl
 

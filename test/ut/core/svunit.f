a885718696fc6ee7c16457b72ed0936cbee0a8d6
Qi Hangkong <qihangkong@outlook.com>
2024-07-04 16:27:12 +0800

    test: move common file project.v to dir test/common

--- test/ut/core/svunit.f
+++ test/ut/core/svunit.f
@@ -1,5 +1,6 @@
 -timescale=1ns/100ps
 +incdir+$SVUNIT_INSTALL/svunit_base/util
++incdir+${RVGPU_HARDWARE}/test/common
 
 -f ${RVGPU_HARDWARE}/rtl/filelists/rvgpu_incdir.f
 -f ${RVGPU_HARDWARE}/rtl/filelists/rvgpu_rtl.fl

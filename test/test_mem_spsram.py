4624aada70962c2e5c76d93a90165dca13e87897
Qi Hangkong <qihangkong@outlook.com>
2024-07-05 15:43:43 +0800

    test: add test_mem_spsram to pytest

---
+++ test/test_mem_spsram.py
@@ -0,0 +1,10 @@
+import os
+import pytest
+import subprocess
+
+from utils import *
+
+@all_files_in_dir('ut/mem')
+@all_available_simulators()
+def test_rvgpu_mem_spsram_256x59(datafiles, simulator):
+    run_testcase(datafiles, simulator, 'ut_rvgpu_spsram_256x59.sv')

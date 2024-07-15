468eecd6c85f1d27016dac4fe92f987175ddccc2
Qi Hangkong <qihangkong@outlook.com>
2024-07-15 19:14:10 +0800

    add licence

--- rtl/core/iu/rcore_iu_top.v
+++ rtl/core/iu/rcore_iu_top.v
@@ -1,5 +1,5 @@
 /*
-Copyright © 2023 Sietium Semiconductor.
+Copyright © 2024 Sietium Semiconductor.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -12,6 +12,10 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+
+Portions of this file are derived from the following projects:  
+ - openc906 (https://github.com/XUANTIE-RV/openc906)  
+   Licensed under the Apache License, Version 2.0 
 */
 
 module rcore_iu_top(
